00368a226003774a2b4ae95bf154f585675987a9
Akira <43845932+PiettroMaximoff@users.noreply.github.com>
2021-09-02 20:52:01 +0300

    Add files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.icon-menu').click(function (event) {
+        $(this).toggleClass('active');
+        $('.menu__body').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+
+});
