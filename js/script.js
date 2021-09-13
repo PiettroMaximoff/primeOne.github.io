1b164f383577147aea42e790acd51647e0602867
Akira <43845932+PiettroMaximoff@users.noreply.github.com>
2021-09-13 13:23:12 +0300

    update

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ $(document).ready(function () {
     $('.icon-menu').click(function (event) {
         $(this).toggleClass('active');
         $('.menu__body').toggleClass('active');
-        $('body').toggleClass('lock');
+        $('body').toggleClass('_lock');
     });
-
-
 });
